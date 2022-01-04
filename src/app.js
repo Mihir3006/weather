e92d72f10d10d267fc044b890e264e24e9aebe89
Mihir Jain <mihirjain766@gmail.com>
2022-01-04 19:29:01 +0530

    setting

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path')
 const app = express();
 const hbs = require('hbs');
-const port =  process.env.PORT || 3000;
+// const port =  process.env.PORT || 8000;
 
 const staticpath = path.join(__dirname,"../public");
 const viewspath = path.join(__dirname, '../templates/views') 
@@ -29,6 +29,4 @@ app.get('*', (req,res) => {
     res.render("error");
 })
 
-app.listen(port, (req,res)=>{ 
-    console.log(`listening to port ${port}`);
-});
+app.listen(process.env.PORT || 8000);
